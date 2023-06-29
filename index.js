51250a45f4ec1dfa0b439409c4d35590b62d4fe8
Bryce <me@brycethedev.com>
2023-06-29 10:15:11 -0400

    Update index.js

--- index.js
+++ index.js
@@ -26,13 +26,7 @@ const limiter = rateLimit({
     res.render('./rate')
       },
 });
-app.use(function(req, res, next) {
-  if (toobusy()) {
-    res.send(503, "I'm busy right now, sorry.");
-  } else {
-    next();
-  }
-});
+
  
 
 app.use(limiter)
